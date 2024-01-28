9c96e5e810f3886c4fe6b9aefa5ebf16f271e50c
J. Nick Koston <nick@koston.org>
2024-01-27 14:00:25 -1000

    feat: add mappings for gzip module

--- src/zlib_fast/__init__.py
+++ src/zlib_fast/__init__.py
@@ -3,14 +3,17 @@ __version__ = "0.1.0"
 import sys
 import zlib as zlib_original
 
+from . import gzip_adapter as best_gzip
 from . import zlib_adapter as best_zlib
 
 
 def enable() -> None:
     """Enable the adapter."""
     sys.modules["zlib"] = best_zlib
+    sys.modules["gzip"] = best_gzip
 
 
 def disable() -> None:
     """Disable the adapter restore the original zlib."""
     sys.modules["zlib"] = zlib_original
+    sys.modules["gzip"] = zlib_original
